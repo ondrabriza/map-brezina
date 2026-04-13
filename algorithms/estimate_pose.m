1d55d514a5584ff85e78032095775f59c2032cf8
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-04-13 20:28:25 +0200

    Week 4 estimated robot position - mean of all particles

--- algorithms/estimate_pose.m
+++ algorithms/estimate_pose.m
@@ -1,7 +1,9 @@
 function [estimated_pose] = estimate_pose(public_vars)
 %ESTIMATE_POSE Summary of this function goes here
 
-estimated_pose = nan(1,3);
+estimated_pose = mean(public_vars.particles);
+
+
 
 end
 
