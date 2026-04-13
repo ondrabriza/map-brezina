4c16506e20437069306a221f391dcbbe04e35f88
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-04-13 20:22:15 +0200

    Week 4 Task 2

--- algorithms/particle_filter/compute_lidar_measurement.m
+++ algorithms/particle_filter/compute_lidar_measurement.m
@@ -1,7 +1,34 @@
 function [measurement] = compute_lidar_measurement(map, pose, lidar_config)
 %COMPUTE_MEASUREMENTS Summary of this function goes here
 
-measurement = zeros(1, length(lidar_config));
+    N = length(lidar_config);
+    measurement = zeros(1, N);
+    
+    ray_origin = pose(1:2);
+    particle_theta = pose(3);
+    walls = map.walls;
+    
+    for i = 1:N
+        ray_direction = particle_theta + lidar_config(i);
+        
+        % normalize to [0, 2*pi]
+        ray_direction = wrapTo2Pi(ray_direction);
+        
+        % calc intersections
+        intersections = ray_cast(ray_origin, walls, ray_direction);
+        
+
+        if ~isempty(intersections)
+            dx = intersections(:, 1) - ray_origin(1);
+            dy = intersections(:, 2) - ray_origin(2);
+            distances = sqrt(dx.^2 + dy.^2);
+
+            measurement(i) = min(distances);
+        else
+            % if the ray hits nothing, set the distance to Inf
+            measurement(i) = Inf;
+        end
+    end
 
 end
 
--- algorithms/particle_filter/weight_particles.m
+++ algorithms/particle_filter/weight_particles.m
@@ -1,8 +1,23 @@
 function [weights] = weight_particles(particle_measurements, lidar_distances)
 %WEIGHT_PARTICLES Summary of this function goes here
+  
+    % set sigma of normal distribution
+    sigma = 0.15; 
+    
+    differences = particle_measurements - lidar_distances;
+    
+    % calc probabilities/weights 
+    probabilities = norm_pdf(differences, 0, sigma); % using normal distribution func we wrote in previous week
+    weights = prod(probabilities, 2);
+    weights(isnan(weights)) = 0;
+    summ = sum(weights);
 
-N = size(particle_measurements, 1);
-weights = ones(N,1) / N;
+    if summ == 0
+        N = size(weights, 1);
+        weights = ones(N, 1) / N;
+    else
+        weights = weights / summ;
+    end
 
 end
 
