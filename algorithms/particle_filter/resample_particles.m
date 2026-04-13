1c948d551c920cd95c5972abb265310914d1020c
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-04-13 20:22:39 +0200

    Week 4 Task 3

--- algorithms/particle_filter/resample_particles.m
+++ algorithms/particle_filter/resample_particles.m
@@ -1,7 +1,29 @@
 function [new_particles] = resample_particles(particles, weights)
 %RESAMPLE_PARTICLES Summary of this function goes here
 
-new_particles = particles;
+    N = size(particles, 1);
+    new_particles = zeros(size(particles));
+
+    % r in range [0, 1/N]
+    r = rand() / N;
+
+    c = weights(1);
+    i = 1;
+
+    for n = 1:N
+        
+        u = r + (n - 1) / N;
+
+        while u > c
+            i = i + 1;
+            c = c + weights(i);
+        end
+
+        noise_vector = [0.05, 0.05, 0.01]; 
+        new_particles(n, :) = particles(i, :) + randn(1, 3) .* noise_vector;
+
+        new_particles(n, 3) = wrapTo2Pi(new_particles(n, 3)); %theta in ragne [0, 2*pi]
+    end
 
 end
 
