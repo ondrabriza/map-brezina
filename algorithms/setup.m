aa5ea99c1e73028f5e12af2035cf0f18bf7486a5
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-04-30 21:28:11 +0200

    Update setup.m: modify start_position and map_name for indoor and outdoor configurations

--- algorithms/setup.m
+++ algorithms/setup.m
@@ -1,20 +1,19 @@
 % start_position = [10, 10, pi/2]; % (x, y, theta)
 % map_name = 'maps/std_map.txt';
 
-% start_position = [1, 1, pi/2]; % (x, y, theta)
-% map_name = 'maps/indoor_3.txt';
+% start_position = [5,5, 5*pi/4]; % (x, y, theta)
+% map_name = 'maps/indoor_1.txt';
 
 % start_position = [4, 2, -pi/4]; % (x, y, theta)
-% map_name = ['maps/outdoor_1.txt'];
+% map_name = ['maps/outdoor_2.txt'];
 
-% start_position = [3, 3, 5*pi/4]; % (x, y, theta)
+% start_position = [11, 2, 5*pi/4]; % (x, y, theta)
 % map_name = ['maps/mixed_1.txt'];
-
-public_vars.mu = [];
-public_vars.sigma = [];
 
 % start_position = [2, 10, 0]; % (x, y, theta)
 % map_name = 'maps/task2_motion_control_map.txt';
 
-start_position = [2, 10, 0]; % (x, y, theta)
-map_name = 'maps/custom_map_1.txt';
+start_position = [6, 6, 0]; % (x, y, theta)
+map_name = 'maps/custom_map_2.txt';
+
+
