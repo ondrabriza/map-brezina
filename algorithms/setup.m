0fef68267ca31663f7de858024e43df0b440c674
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-04-30 12:32:03 +0200

    Update setup.m to comment out previous map configurations and add custom_map_1.txt

--- algorithms/setup.m
+++ algorithms/setup.m
@@ -4,11 +4,17 @@
 % start_position = [1, 1, pi/2]; % (x, y, theta)
 % map_name = 'maps/indoor_3.txt';
 
-start_position = [4, 2, -pi/4]; % (x, y, theta)
-map_name = 'maps/outdoor_2.txt';
+% start_position = [4, 2, -pi/4]; % (x, y, theta)
+% map_name = ['maps/outdoor_1.txt'];
+
+% start_position = [3, 3, 5*pi/4]; % (x, y, theta)
+% map_name = ['maps/mixed_1.txt'];
 
 public_vars.mu = [];
 public_vars.sigma = [];
 
 % start_position = [2, 10, 0]; % (x, y, theta)
 % map_name = 'maps/task2_motion_control_map.txt';
+
+start_position = [2, 10, 0]; % (x, y, theta)
+map_name = 'maps/custom_map_1.txt';
