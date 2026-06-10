d0c63c5cde4629d2c2ea09db1d359b78a6a587e7
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-06-10 08:36:57 +0200

    Update start_position and map_name for indoor scenario in setup.m

--- algorithms/setup.m
+++ algorithms/setup.m
@@ -1,8 +1,8 @@
 % start_position = [10, 10, pi/2]; % (x, y, theta)
 % map_name = 'maps/std_map.txt';
 
-% start_position = [5,5, 5*pi/4]; % (x, y, theta)
-% map_name = 'maps/indoor_1.txt';
+% start_position = [1,2, 5*pi/4]; % (x, y, theta)
+% map_name = 'maps/indoor_3.txt';
 
 % start_position = [4, 2, -pi/4]; % (x, y, theta)
 % map_name = ['maps/outdoor_2.txt'];
